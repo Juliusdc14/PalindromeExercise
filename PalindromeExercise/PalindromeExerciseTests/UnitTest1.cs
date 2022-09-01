6a3965906d5c5f86198f018042410ac5f9b8523a
juliusdc14 <juliusdc05@gmail.com>
2022-09-01 11:07:51 -0500

    completed unit test

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("Racecare", false)]
+        public void Mytest(string word, bool expected)
         {
+            //Arrange
+            var myTest = new WordSmith();
+
+            //Act
+            bool actual = myTest.IsPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
